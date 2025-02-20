88cd46e1568346d101f2b1a7fc2b693d246925f8
Mandeepk8-ai <mkaur588540@gmail.com>
2025-02-20 09:55:49 +0000

    Add files via upload

---
+++ relational.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter number a:");
+scanf("%d",&a);
+printf("enter number b:");
+scanf("%d",&b);
+printf("greater :%d\n",a>b);
+printf("greater than :%d\n",a>=b);
+printf("less than :%d\n",a<=b);
+printf("equal :%d\n",a==b);
+printf("not equal :%d\n",a!=b);
+return 0;
+}
