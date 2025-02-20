fcba98296115ab15be6880b9029db0ab533e9ccf
Mandeepk8-ai <mkaur588540@gmail.com>
2025-02-20 09:25:07 +0000

    Add files via upload

---
+++ Arithmatic operator.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a=9;
+    int b=6;
+    printf("Addition is %d\n",a+b);
+    printf("Substraction is %d\n",a-b);
+    printf("Multiplication is %d\n",a*b);
+    
+    printf("Division is %d\n",a/b);
+    printf("Modules is %d\n",a%b);
+    return 0;
+}
